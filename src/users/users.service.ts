8a85e6013bb060df0fb4f6f564f986bf027a59a2
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2024-11-11 20:01:50 +0100

    feat: update create, find, update user service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Injectable,
+  HttpException,
+  HttpStatus,
+  BadRequestException,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -21,48 +29,125 @@ export class UsersService {
 
   /** Create user */
   async create(user: ICreateUser) {
-    const existingUser = await this.usersRepository.findOne({
-      where: {
-        login: user.login,
-      },
-    });
+    let existingUser = null;
+
+    try {
+      existingUser = await this.usersRepository.findOne({
+        where: {
+          login: user.login,
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to retrieve users from the database',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
+
+    if (existingUser) {
+      throw new BadRequestException('User with this login already exists');
+    }
 
     let newUser = this.usersRepository.create(user);
-    newUser = await this.usersRepository.save(newUser);
+
+    try {
+      newUser = await this.usersRepository.save(newUser);
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to create new user', {
+        description: 'Error connecting to the database',
+      });
+    }
 
     return newUser;
   }
 
   /** Find all Users */
-  findAll(): ICreateUser[] {
-    return this.users;
+  async findAll(): Promise<ICreateUser[]> {
+    let users = null;
+
+    try {
+      users = await this.usersRepository.find({
+        select: ['id', 'login'],
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to retrieve users from the database',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
+
+    return users;
   }
 
   /** Find 1 User by ID */
-  findOne(id: string) {
-    return this.users.find((user: ICreateUser) => id === user.id);
+  async findOne(id: string) {
+    let user = null;
+
+    try {
+      user = await this.usersRepository.findOne({
+        where: { id },
+        select: ['id', 'login'],
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to retrieve user from the database',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
+
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+
+    return user;
   }
 
   /** Update password */
-  updatePassword(id: string, updatePasswordDto: IUpdatePassword) {
-    const user = this.findOne(id);
+  async updatePassword(id: string, updatePasswordDto: IUpdatePassword) {
+    let user = null;
+    try {
+      user = await this.usersRepository.findOne({
+        where: { id },
+        select: ['id', 'password'],
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to retrieve user from the database',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
+
     if (!user) {
-      throw new HttpException('User doesn`t exist', HttpStatus.NOT_FOUND);
+      throw new NotFoundException('User not found!');
     }
 
     const isMatch = updatePasswordDto.oldPassword === user.password;
-
     if (!isMatch) {
-      throw new HttpException('Incorrect password', HttpStatus.FORBIDDEN);
+      throw new UnauthorizedException('Incorrect password');
     }
 
-    this.users = this.users.map((user) =>
-      user.id === id
-        ? { ...user, password: updatePasswordDto.newPassword }
-        : user,
-    );
+    try {
+      await this.usersRepository.update(id, {
+        password: updatePasswordDto.newPassword,
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to update password in the database',
+        {
+          description: 'Error updating the password in the database',
+        },
+      );
+    }
 
-    return { id, message: 'Password updated successfully' };
+    return { id, message: 'Password updated successfully', status: 'success' };
   }
 
   /** Delete user */
